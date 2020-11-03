f269ad24748202c32adc43c210fb250a7a9deb79
Josh Errickson <josh@errickson.net>
2020-11-03 05:30:14 -0500

    Updating license information

--- R/rrelaxiv-package.R
+++ R/rrelaxiv-package.R
@@ -1,3 +1,7 @@
 #' @useDynLib rrelaxiv, .registration=TRUE
 NULL
 #> NULL
+
+.onAttach <- function(lib, pkg) {
+packageStartupMessage("The rrelaxiv package has an academic license. Enter relaxinfo() for more information.")
+}
