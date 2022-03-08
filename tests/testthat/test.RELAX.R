a101c88dc4e76921e1817f35308211f6b6700c43
Josh Errickson <josh@errickson.net>
2022-03-07 20:41:10 -0500

    Expanded tests slightly

--- tests/testthat/test.RELAX.R
+++ tests/testthat/test.RELAX.R
@@ -1,4 +1,4 @@
-test_that("Basic test", {
+test_that("Basic test of RELAX-IV", {
   data(relaxexample)
   results <- RELAX_IV(relaxexample$startnodes,
                       relaxexample$endnodes,
@@ -9,5 +9,27 @@ test_that("Basic test", {
   expect_true(is.numeric(results))
   expect_true(all(results) >= 0)
   expect_equal(length(results),
+               length(relaxexample$startnodes))
+})
+
+test_that("Basic test of .RELAX-IV", {
+  data(relaxexample)
+  re <- relaxexample
+  results <- .RELAX_IV(
+    n1 = as.integer(length(unique(c(re$startnodes, re$endnodes)))),
+    na1 = as.integer(length(re$startnodes)),
+    startn1 = as.integer(re$startnodes),
+    endn1 = as.integer(re$endnodes),
+    c1 = as.integer(re$arccosts),
+    u1 = as.integer(re$arccapacity),
+    b1 = as.integer(re$nodedemand),
+    rc1 = as.integer(re$arccosts), # No reduced costs for now
+    crash1 = as.integer(0),
+    large1 = as.integer(.Machine$integer.max / 4))
+
+
+  expect_true(is.numeric(results$x1))
+  expect_true(all(results$x1) >= 0)
+  expect_equal(length(results$x1),
                length(relaxexample$startnodes))
 })
